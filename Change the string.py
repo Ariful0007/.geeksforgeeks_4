a7c12b1f2fecaac9c1461e49918e4117c63a13eb
Ariful islam <55908839+Ariful0007@users.noreply.github.com>
2020-04-15 18:57:08 -0700

    Create Change the string.py

---
+++ Change the string.py
@@ -0,0 +1,22 @@
+
+
+
+
+for _ in range(int(input())):
+    str1 = str(input())
+    str2 = str1[0:1]
+    if str2>='A' and   str2 <='Z':
+        print(str1.upper())
+    else : 
+        print(str1.lower())
+
+   # print(str2)
+
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
